fa5cb22d375b9f9b5e18297470291146b5f82a62
Manjusaikrishna <58949860+Manjusaikrishna@users.noreply.github.com>
2021-05-12 01:26:08 +0530

    Update HeartDisease_prediction_for_webapp.py

--- HeartDisease_prediction_for_webapp.py
+++ HeartDisease_prediction_for_webapp.py
@@ -12,16 +12,49 @@ import requests
 import io
 
 st.title("""
-# Heart Disease Prediction App
-
-This app predicts whether the person having given values of parameters based on tests is having heart disease or not type!
+Heart Disease Prediction App
+This app predicts whether the person having given values of parameters based on tests is having heart disease or not type
 """)
+st.write("""
+Attribute documentation:\n
+1. age: age in years\n
+2. sex: sex (1 = male; 0 = female)\n
+3. cp: chest pain type\n
+-- Value 1: typical angina
+-- Value 2: atypical angina
+-- Value 3: non-anginal pain
+-- Value 4: asymptomatic
+4. trestbps: resting blood pressure (in mm Hg on admission to the hospital)\n
+5. chol: serum cholestoral in mg/dl\n
+6. fbs: (fasting blood sugar > 120 mg/dl) (1 = true; 0 = false)\n
+7. restecg: resting electrocardiographic results\n
+-- Value 0: normal
+-- Value 1: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)
+-- Value 2: showing probable or definite left ventricular hypertrophy by Estes' criteria
+8. thalach: maximum heart rate achieved\n
+9. exang: exercise induced angina (1 = yes; 0 = no)\n
+10. oldpeak = ST depression induced by exercise relative to rest\n
+11. slope: the slope of the peak exercise ST segment\n
+-- Value 1: upsloping
+-- Value 2: flat
+-- Value 3: downsloping
+12. ca: number of major vessels (0-3) colored by flourosopy\n
+13. thal: 3 = normal; 6 = fixed defect; 7 = reversable defect\n
+14. num: diagnosis of heart disease (angiographic disease status)
+-- Value 0: < 50% diameter narrowing
+-- Value 1: > 50% diameter narrowing
+"""
+)
 
 st.sidebar.header("User Input Parameters")
 
 def user_input_features():
     age = st.sidebar.slider("age",20,100,55)
-    sex=st.sidebar.slider("sex",0,1,1)
+    sex = st.sidebar.selectbox("Select the Gender",("Male", "Female"))
+    if sex =='Male':
+        gender = 1 
+    else :
+        gender = 0
     cp=st.sidebar.slider("cp",0,3,2)
     trestbps=st.sidebar.slider("trestbps",94,200,150)
     chol=st.sidebar.slider("chol",126,138,128)
@@ -34,7 +67,7 @@ def user_input_features():
     ca=st.sidebar.slider("ca",0,4,3)
     thal=st.sidebar.slider("thal",0,3,1)
     data = {"age": age,
-            "sex": sex,
+            "sex": gender,
             "cp": cp,
             "trestbps": trestbps,
             "chol": chol,
@@ -48,7 +81,6 @@ def user_input_features():
             "thal": thal}
     features = pd.DataFrame(data,index=[0])
     return features
-
 df = user_input_features()
 
 st.subheader("User Input Parameters")
